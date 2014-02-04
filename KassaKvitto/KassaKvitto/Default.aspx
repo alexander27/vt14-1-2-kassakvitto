1243040e6d76212eaf166abbf3354b8d1767b6a9
alexander27 <ad222in@student.lnu.se>
2014-02-04 17:12:28 +0100

    kassak

---
+++ KassaKvitto/KassaKvitto/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="KassaKvitto.Default" ViewStateMode="Disabled" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>KassaKvitto</title>
+    <link href="Style.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>KassaKvitto</h1>
+    <form id="form1" runat="server">
+        <div id="topBar">
+        </div>
+       <p>
+         <asp:Label ID="Label1" runat="server" Text="Total Köpesumma"></asp:Label>
+       </p>  
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox> 
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Kr"></asp:Label>
+        </p>
+        <asp:Button ID="Button1" runat="server" Text="Beräkna rabatt" OnClick="Button1_Click" />
+        
+        
+        
+        <div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ KassaKvitto/KassaKvitto/Model/Receipt.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KassaKvitto.Model
+{
+    public class Receipt
+    {
+        public static int GetTotalAmount(string input)
+        {
+
+         private double _subtotal;
+
+        
+        public double DisckountRate
+        {
+            get{
+                return _subtotal;
+            }
+           private set{
+            
+            }
+          
+        }
+
+       public double MoneyOf
+       {
+       get{
+       
+       }
+          private set{
+           
+           }
+       }
+        
+        public  double Subtotal
+            {
+              public   get{
+                     return _subtotal;
+                 
+                    }
+             private   set{
+                            if(value < 0)
+                          {
+                                throw new ArgumentException("Fel fel");
+                          }
+                            
+                            _subtotal = value;
+                          }
+          }           
+         public double Total
+         {
+            get{
+
+            }
+            set{
+               }
+         }
+    
+    
+    }
+  
+}
